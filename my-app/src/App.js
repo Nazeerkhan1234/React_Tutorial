c325cacd143ea94a590669758222fdf731fdb8cf
nazeerkhan12 <nazeer.1973.cs@ieccollege.com>
2023-04-02 13:32:54 +0530

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+      <h1>hello</h1>
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
