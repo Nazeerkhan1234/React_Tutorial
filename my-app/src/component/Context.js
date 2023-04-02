20a7a5d5640258161ebd8b0e57cd3ab518ccabb3
nazeerkhan12 <nazeer.1973.cs@ieccollege.com>
2023-04-03 01:21:01 +0530

    useContext

---
+++ my-app/src/component/Context.js
@@ -0,0 +1,53 @@
+import React, { useState, useContext } from "react";
+
+let context = createContext(); // create context
+
+function Context() {
+  const [fName, setfName] = useState("Tony");
+  const [lName, setlName] = useState("Stark");
+
+  return (
+    
+      <context.Provider value={{fName , lName}}>
+        <h3>This is Parent Compoenet</h3>
+        <ChildA/>
+      
+      </context.Provider>
+
+      // Providing Props
+   
+  );
+}
+
+function ChildA() {
+  return (
+    <>
+      <h3>This is child A</h3>
+
+      <ChildB/>
+    </>
+  );
+}
+
+function ChildB() {
+  return (
+    <>
+      <h3>This is child B</h3>
+     
+
+      <ChildC />
+    </>
+  );
+}
+
+function ChildC() {
+      const {fName , lName}= useContext(context) // Consumimg props with useContext
+  return (
+    <>
+      <h3>This is child C</h3>
+      <h1>{fName} {lName}</h1>
+      
+    </>
+  );
+}
+export default Context;
